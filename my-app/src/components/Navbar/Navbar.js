2ef16b65b5627e02ea2058b5c3e2ba220ddda73c
Cesar Ochoa Navarro <cesarochoan@gmail.com>
2021-11-23 00:26:09 -0800

    almost complete

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -3,14 +3,19 @@ import './Navbar.css';
 import { MenuItems } from './MenuItems';
 
 class Navbar extends Component {
+  state = { active: false }
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
   render () {
     return(
       <nav className="NavbarItems">
         <h1 className="navbar-logo">FastStats</h1>
-        <div className="menu-icons">
-
+        <div className="menu-icon" onClick={this.handleClick}>
+            <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
-        <ul>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
           {MenuItems.map((item, index) => {
               return (
                 <li key={index}>
