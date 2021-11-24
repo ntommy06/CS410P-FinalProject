b1071cd4459246feaca8a1aa041d429c56c23606
Cesar Ochoa <cesarochoa@Cesars-MacBook-Air.local>
2021-11-23 16:22:54 -0800

    Completed NavBar

---
+++ my-app/src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Search.css';
+
+const Search = () => {
+    return (
+        <div className="mySearch">
+            <h1>Search Page</h1>
+        </div>
+    )
+}
+
+export default Search;
