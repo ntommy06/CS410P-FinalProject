9bfce54b6991e8e6957669781e1945292de08ae3
Tommy Nguyen <tommynguyen@Tommys-MacBook-Pro.local>
2021-11-23 16:28:10 -0800

    added img

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import Compare from './pages/Compare';
-import Performance from './pages/Performance';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Compare from "./pages/Compare";
+import Performance from "./pages/Performance";
 
 function App() {
   return (
@@ -14,11 +13,11 @@ function App() {
       <div className="header"></div>
       <div className="navBar_class">
         <Navbar></Navbar>
-        {/* <img
+        <img
           id="mjWings"
           src="./mj_wings.jpeg"
           alt="Backetball Player Image"
-        ></img> */}
+        ></img>
       </div>
       <div className="mainContent">
         <h2>FastStats Features</h2>
@@ -27,7 +26,6 @@ function App() {
         <h2>Developers of FastStats</h2>
       </div>
     </body>
-
   );
 }
 
