87b309b9f661fa015fda4a17eaa2d303642f3e18
Cesar Ochoa Navarro <cesarochoan@gmail.com>
2021-11-24 15:15:08 -0800

    got api to display name and team for two players

--- my-app/src/pages/Compare.js
+++ my-app/src/pages/Compare.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Compare.css'
 import axios from 'axios';
 
 class Compare extends Component {
-
     constructor(props) {
         super(props);
         this.state= {
-            playerName: null,
-            playerStats: {}
+            playerName: [null, null],
+            playerAvg: {}
         }
     }
 
@@ -16,9 +15,15 @@ class Compare extends Component {
         let player1 = document.getElementById("compare1").value;
         // console.log(getName);
         
-        axios.get("https://www.balldontlie.io/api/v1/players?search=" + player1)
+        axios.get(`https://www.balldontlie.io/api/v1/players?search=${player1}`)
         .then(async res => {
             console.log(res.data.data)
+            let realName = res.data.data[0].first_name + " " + res.data.data[0].last_name;
+            let team = res.data.data[0].team.full_name;
+            document.getElementById("p1Name").innerHTML = realName;
+            document.getElementById("p1Team").innerHTML = team;
+
+            await this.testD(0, realName)
             await this.getAvgStats(res.data.data[0].id)
         }).catch(err => {
             console.log(err)
@@ -29,9 +34,16 @@ class Compare extends Component {
         let player2 = document.getElementById("compare2").value;
         // console.log(getName);
         
-        axios.get("https://www.balldontlie.io/api/v1/players?search=" + player2)
+        axios.get(`https://www.balldontlie.io/api/v1/players?search=${player2}`)
         .then(async res => {
             console.log(res.data.data)
+            let realName = res.data.data[0].first_name + " " + res.data.data[0].last_name;
+            let team = res.data.data[0].team.full_name;
+            document.getElementById("p2Name").innerHTML = realName;
+            document.getElementById("p2Team").innerHTML = team;
+
+            
+            await this.testD(1, realName)
             await this.getAvgStats(res.data.data[0].id)
         }).catch(err => {
             console.log(err)
@@ -40,32 +52,56 @@ class Compare extends Component {
 
     getAvgStats = (playerid) => {
         console.log(playerid);
+        axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${playerid}`)
+        .then(async res => {
+            console.log(res.data.data)
+            this.setState({playerAvg: res.data.data[0]})
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
     // componentDidMount() {
     //     this.getPlayerId();
     // }
 
+    testD = (index, name) => {
+        // console.log(this.state.playerName)
+    }
+    
     render() {
+        
         return (
             <>
             <div className='myCompare'>
-                <h1 id='myCompare'>Search up two players you would like to compare</h1>
+                <h1>Search up two players you would like to compare</h1>
             </div>
-            <input
-                type="search"
-                id="compare1"
-                placeholder="Enter first player name"
-            />
-            <button type="submit" onClick={this.getPlayerId1}>Enter</button>
-            
-            <input
-                type="search"
-                id="compare2"
-                placeholder="Enter first player name"
-            />
-            <button type="submit" onClick={this.getPlayerId2}>Enter</button>
+            <div className="search">
+                <input
+                    type="search"
+                    id="compare1"
+                    placeholder="Enter first and last name"
+                    name="input"
+                />
+                <button type="submit" id="btn" onClick={this.getPlayerId1}>Enter</button>
 
+                <input
+                    type="search"
+                    id="compare2"
+                    placeholder="Enter first and last name"
+                    name="input"
+                />
+                <button type="submit" id="btn" onClick={this.getPlayerId2}>Enter</button>
+            </div>
+            {/* SEARCH HOW TO CREATE A TABLE NEXT TO PUT THESE ELEMENTS INTO THE TABLE FOR COMPARISON */}
+            <div>
+                <p>Name: <span id="p1Name"></span> </p>
+                <p>Team: <span id="p1Team"></span> </p>
+            </div>
+            <div>
+                <p>Name: <span id="p2Name"></span> </p>
+                <p>Team: <span id="p2Team"></span> </p>
+            </div>
             </>
         )
     }
