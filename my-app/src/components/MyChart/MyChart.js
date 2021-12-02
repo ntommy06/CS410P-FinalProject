9fc01d52b094fbe8ff3d13ff8fe66e3f13ce660d
Cesar Ochoa <cesarochoan@gmail.com>
2021-12-01 21:28:21 -0800

    Completed daily Schedule

--- my-app/src/components/MyChart/MyChart.js
+++ my-app/src/components/MyChart/MyChart.js
@@ -1,33 +1,46 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 import './MyChart.css'
+import axios from "axios";
 import {Bar, Chart, Line, Pie, Radar} from 'react-chartjs-2'
 import { BarElement } from 'chart.js/auto'
 import { plugins } from '@testing-library/react/node_modules/pretty-format'
 
 
-    function MyChart(){
-        return <div id="barChart">
-            <Radar
-                datasetIdkey='id'
-                data={{
-                    labels: ['FGM', 'FGA', 'FG%', 'FTM', 'FTA', 'FT%', '3PTM', '3PTA', '3PT%', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'TO'],
-                    datasets: [
-                        {
-                            id: 2,
-                            label: 'Player Stats',
-                            data: [3, 10, 1, 4, 5, 6, 4, 8, 9, 10, 11, 2, 2, 14, 5],
-                        },
-                    ],
-                }}
-                width={800}
-                height={400}
-                options={{
-                    maintainAspectRatio: false
-                }}
+class MyChart extends Component {
 
-            />
-        </div>
+    constructor(props){
+        super(props);
+        this.state={
+            playerName:[],
+            playerAvg:[]
+        }
     }
 
+    render(){
 
-    export default MyChart
+        return(
+            <div id="barChart">
+                <Radar
+                    datasetIdkey='id'
+                    data={{
+                    labels: ['FGM', 'FGA', 'FG%', 'FTM', 'FTA', 'FT%', '3PTM', '3PTA', '3PT%', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'TO'],
+                    datasets: [{
+                        id: 2,
+                        label: 'Player Stats',
+                        data: [3, 10, 1, 4, 5, 6, 4, 8, 9, 10, 11, 2, 2, 1, 5],
+                    },],
+                    }}
+                    width={800}
+                    height={400}
+                    options={{
+                        maintainAspectRatio: false
+                    }}
+                />
+            </div>
+        );
+    }
+
+}
+
+
+export default MyChart;
